7a00e96956cef9ec4288255625ac78b5e57598ff
Lia Kahn <liakahn@Lias-MacBook-Pro.local>
2023-09-25 09:40:22 -0600

    added new delete route

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const db = require('./db/db.json')
 const app = express();
 const fsUtils = require('./helpers/fsUtils');
 const uuid = require('./helpers/uuid')
-const fs = require('fs');
+
 
 
 
@@ -16,62 +16,38 @@ app.use(express.static('public'));
 
 
 //html routes
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public.index.html'))
-})
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-
-app.get('/api/notes', (req, res) => {
-  sendFile(path.join(__dirname, '/public/notes.html'))
-
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 
 //api routes
-
 app.get("/api/notes", (req, res) => {
   fsUtils.readFromFile("./db/db.json").then((data) => {
-    console.log(JSON.parse(data));
+    // console.log(JSON.parse(data));
     res.json(JSON.parse(data));
   });
 });
 
-
-
-app.post('/api/notes', (req, res) => {
-  console.info(`${req.method} request received to add a note`);
-
-  // Destructuring assignment for the items in req.body
+app.post("/api/notes", (req, res) => {
   const { title, text } = req.body;
-
-  // Variable for the object we will save
   const userNote = {
-    title,
-    text,
-    note_id: uuid(),
+    title: title,
+    text: text,
+    id: uuid(),
   };
-
-  fsUtils.readAndAppend(userNote, './db/db.json');
+  fsUtils.readAndAppend(userNote, "./db/db.json");
   console.log(userNote);
-  res.json('Note has been added!');
-
-
-
+  res.json("Note has been added!✅");
 });
 
-//
-
-app.delete('/api/notes/:id', (req, res) => {
-  fsUtils.deleteAndAppend(req.params.id, './db/db.json')
-  res.json('Note has been deleted!')
-
-})
-
-
+app.delete("/api/notes/:id", (req, res) => {
+  fsUtils.deleteAndAppend(req.params.id, "./db/db.json")
+  res.json("Note has been deleted!🗑️ ")
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
